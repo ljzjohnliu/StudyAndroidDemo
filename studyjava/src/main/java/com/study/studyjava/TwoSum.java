9268ae9ad4f446641173ac29fa22b31d1603ba7b
ljz <liujianzhang.901@bytedance.com>
2025-10-17 14:27:51 +0800

    leecode：两数之和

---
+++ studyjava/src/main/java/com/study/studyjava/TwoSum.java
@@ -0,0 +1,42 @@
+package com.study.studyjava;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * 你可以按任意顺序返回答案。
+ */
+public class TwoSum {
+    /**
+     * 借助Map只需要一次循环
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target-nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 双层循环
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            result[0] = i;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
